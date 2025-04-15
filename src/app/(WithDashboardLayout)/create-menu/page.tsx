d6544c2612998d05cfd72cb0976fff8b43bf517c
Md Sujon Mia <sujon258549@gmail.com>
2025-04-15 21:45:48 +0600

    order create and update

---
+++ src/app/(WithDashboardLayout)/create-menu/page.tsx
@@ -0,0 +1,11 @@
+import MenuAddForm from "@/components/modules/menu/createmenu/CreateMenu";
+
+const AddMenu = () => {
+  return (
+    <div>
+      <MenuAddForm />
+    </div>
+  );
+};
+
+export default AddMenu;
