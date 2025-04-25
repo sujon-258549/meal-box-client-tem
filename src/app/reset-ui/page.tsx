45d4e82ee6f434432595b2ec6eebb0166086be32
kmdripon1991 <kmdripon1991@gmail.com>
2025-04-25 23:19:47 +0600

    update

--- src/app/reset-ui/page.tsx
+++ src/app/reset-ui/page.tsx
@@ -1,54 +1,7 @@
-// // import { RestUiForm } from "@/components/modules/user/RestUiForm";
-
-// import { RestUiForm } from "@/components/modules/user/RestUiForm";
-
-// // const ResetUiPage = ({
-// //   searchParams,
-// // }: {
-// //   searchParams: { token?: string; email?: string };
-// // }) => {
-// //   if (!searchParams.token || !searchParams.email) {
-// //     return <div>Invalid reset link</div>;
-// //   }
-
-// //   return (
-// //     <div>
-// //       <RestUiForm token={searchParams.token} email={searchParams.email} />
-// //     </div>
-// //   );
-// // };
-
-// // export default ResetUiPage;
-
-// interface ResetUiPageProps {
-//   searchParams: {
-//     token?: string;
-//     email?: string;
-//   };
-// }
-
-// const ResetUiPage = ({ searchParams }: ResetUiPageProps) => {
-//   if (!searchParams.token || !searchParams.email) {
-//     return <div>Invalid reset link</div>;
-//   }
-
-//   return (
-//     <div>
-//       <RestUiForm token={searchParams.token} email={searchParams.email} />
-//     </div>
-//   );
-// };
-
-// export default ResetUiPage;
-
-import React from 'react';
+import React from "react";
 
 const ResetUiPage = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ResetUiPage;
